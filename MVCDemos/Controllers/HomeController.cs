9bb4efe9dc52bf8526f7581412ef32a4b2c25fc7
aditimarathe <aditimarathe1@gmail.com>
2018-02-26 16:57:37 +0530

    First mvc commit

---
+++ MVCDemos/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+
+namespace MVCDemos.Controllers
+{
+    using MVCDemos.Models;
+
+    public class HomeController : Controller
+    {
+
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            var u = new User();
+
+            u.UserName = "darshan";
+            u.FirstName = "darshan";
+            u.LastName = "Marathe";
+
+            return View(u);
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(User u)
+        {
+            return View(u);
+        }
+    }
+}
